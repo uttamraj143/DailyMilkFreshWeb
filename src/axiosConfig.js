bbc633d9a9fa9e9322c1fc630744576fa9bb7594
chai <chaitanya.arige@gmail.com>
2021-09-30 23:29:46 +0530

    finish agent list

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const baseURL =
   // process.env.REACT_APP_HOSTNAME ||
-  // "http://dailyorganicfresh.dailyfresh.farm";
-  "http://dailyfreshmilk-980670318.us-east-1.elb.amazonaws.com/";
+  "http://dailyorganicfresh.dailyfresh.farm";
+// "http://dailyfreshmilk-980670318.us-east-1.elb.amazonaws.com/";
 
 export const axiosInstance = axios.create({
   baseURL: baseURL,
