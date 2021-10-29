8dc38d7a0fe9536f7b82f697654205094a4e6878
krige <chaitanya.arige@gmail.com>
2021-10-29 13:12:12 +0530

    delivery types assign

---
+++ src/components/AssigningUsersToAgent/SelectUser.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
+import { listUsers } from "store/user";
+import CheckBoxOutlineBlankIcon from "components/svgs/unchecked.svg";
+import CheckBoxIcon from "components/svgs/checked.svg";
+
+const icon = <img src={CheckBoxOutlineBlankIcon} alt="dsd" fontSize="small" />;
+const checkedIcon = <img src={CheckBoxIcon} alt="dsds" fontSize="small" />;
+<CheckBoxIcon fontSize="small" />;
+
+export default function SelectUser(props) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getAllUsers = () => {
+      listUsers(3, props.access_token)
+        .then((res) => {
+          toggleSpinner(false);
+          setUsers(res.data.data);
+        })
+        .catch((res) => {
+          if (res && res.response && res.response.status === 401) {
+            toggleSpinner(true);
+            props.refreshAccessToken();
+          }
+        });
+    };
+    getAllUsers();
+  }, []);
+
+  return (
+    <div className="AssignUsers__main">
+      <Autocomplete
+        // multiple
+        id="checkboxes-tags-demo"
+        options={users}
+        disableCloseOnSelect
+        getOptionLabel={(option) => option.name}
+        renderOption={(props, option, { selected }) => (
+          <li {...props}>
+            <Checkbox
+              icon={icon}
+              checkedIcon={checkedIcon}
+              style={{ marginRight: 8 }}
+              checked={selected}
+            />
+            {option.name}
+          </li>
+        )}
+        renderInput={(params) => (
+          <TextField {...params} label="Checkboxes" placeholder="Favorites" />
+        )}
+      />
+    </div>
+  );
+}
--- src/components/Products/ProductsListing.js
+++ src/components/Products/ProductsListing.js
@@ -1,41 +1,62 @@
 import Paper from "@mui/material/Paper";
+import { useState } from "react";
+import { updateProduct } from "store/products";
 
-export default function OrdersListing(props) {
+export default function ProductsListing(props) {
+  const [product, setProduct] = useState(props.products);
+  const orderStatusColor = (status) => {
+    if (status === 1) return "Orders__blue";
+    if (status === 0) return "Orders__yellow";
+    return "Orders__grey";
+  };
+
+  const changeAvailability = (e) => {
+    let newff = product.availability === 0 ? 1 : 0;
+
+    e.preventDefault();
+    setProduct({
+      ...product,
+      availability: newff,
+    });
+
+    let data = {
+      availability: newff.toString(),
+    };
+    updateProduct(props.access_token, product.id, data);
+  };
+
   return (
     <div className="Products__card-container">
-      {props.products.map((product, index) => {
-        return (
-          <Paper key={index} className="Products__order" elevation={2}>
-            <div className="Products__customername">
-              <div className="Products__cust-id">In Stock </div>
-              <div className={"Products__status "}>
-                {product.availability === 1 ? "Available" : "Not available"}
-              </div>
-            </div>
+      <Paper className="Products__order" elevation={2}>
+        <div className="Products__customername">
+          <div className="Products__cust-id">In Stock </div>
+          <div
+            onClick={(e) => changeAvailability(e)}
+            className={
+              "Products__status " + orderStatusColor(product.availability)
+            }
+          >
+            {product.availability === 1 ? "Available" : "Not available"}
+          </div>
+        </div>
 
-            <div className="Products__products-row">
-              <div className="Products__cust-heading">Name</div>
-              <div className="Products__right-column">{product.name}</div>
-            </div>
-            <div className="Products__products-row">
-              <div className="Products__date">Description</div>
-              <div className="Products__right-column">
-                {product.description}
-              </div>
-            </div>
-            <div className="Products__products-row">
-              <div className="Products__product-id">Price</div>
-              <div className="Products__right-column">{product.price}</div>
-            </div>
-            <div className="Products__products-row">
-              <div className="Products__date">Product Type</div>
-              <div className="Products__right-column">
-                {product.product_type}
-              </div>
-            </div>
-          </Paper>
-        );
-      })}
+        <div className="Products__products-row">
+          <div className="Products__cust-heading">Name</div>
+          <div className="Products__right-column">{product.name}</div>
+        </div>
+        <div className="Products__products-row">
+          <div className="Products__date">Description</div>
+          <div className="Products__right-column">{product.description}</div>
+        </div>
+        <div className="Products__products-row">
+          <div className="Products__product-id">Price</div>
+          <div className="Products__right-column">{product.price}</div>
+        </div>
+        <div className="Products__products-row">
+          <div className="Products__date">Product Type</div>
+          <div className="Products__right-column">{product.product_type}</div>
+        </div>
+      </Paper>
     </div>
   );
 }
